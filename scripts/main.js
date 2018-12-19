32d0f9c14aeba85d9606e4a800817832516e65af
Aerithe77 <gouesbet.vincent@gmail.com>
2018-12-19 15:56:18 +0100

    ajoue popup en js

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+setTimeout(popup, 10000);
+function popup() {
+  if (confirm("Hey petit Billy ! Oui toi ! Viens découvrir l'underground du net sur xxxvidsxxx ! Peut-être y trouveras tu des réponses à tes questionnements... Suis le lapin Rose !")) {
+        document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+   }
+   else {
+     return false
+   }
+};
